b8d96c2e1f34b987a51f380f1e0a2d73220818ec
[NicholasJamou1] <[s3783740@student.rmit.edu.au]>
2020-03-10 17:23:59 +1100

    Inital Commit

---
+++ src/week1/main.java
@@ -0,0 +1,24 @@
+package week1;
+
+public class main {
+	
+	public static void main(String[] args) {
+		int[] items= new int[5];
+		bubbleSort(items);
+	}
+
+	public static void bubbleSort(int[] items) {
+		
+		int n = items.length;
+		
+		for (int i = 0 ; i<n-1 ;  i++) {
+			
+		}
+		
+		
+		
+	}
+	private void Swap(int a, int b) {
+		
+	}
+}
